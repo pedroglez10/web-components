f696e1fc7f2cec4d67c4d2c24190ecdc2139012b
Pedro Garcia Glez <garciaglez9210@gmail.com>
2023-05-26 19:45:29 -0700

    first commit

---
+++ CustomElements/my-element.js
@@ -0,0 +1,27 @@
+const template = document.createElement('div');
+template.innerHTML = `
+    <style>
+        .marked {
+            background: yellow;
+        }
+    </style>
+    <h2>Este es un template</h2>
+    <p class="marked">ejemplo de un template.</p>
+`;
+
+class myElement extends HTMLElement {
+    // inicializar todo lo que esta en memoria
+    constructor() {
+        super(); // acceder a los metodos de HTMLElement
+        console.log("Soy un web component");
+
+        this.p = document.createElement('p'); // crea una nueva etiqueta en memoria
+    }
+
+    connectedCallback() {
+        this.p.textContent = "Hola mundo."; // establece contenido en texto
+        this.appendChild(this.p); // inserta la etiqueta que estab en memoria al DOM
+        this.appendChild(template);
+    }
+}
+customElements.define('my-element', myElement); // define el nombre de la etiqueta que sera usada en el maquetado
